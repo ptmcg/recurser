b6267e3c17a3be3577cc394d366db71a6e3fe5a2
ptmcg <ptmcg@austin.rr.com>
2019-08-10 17:16:16 -0500

    Test for all those changes

--- test_interpreter.py
+++ test_interpreter.py
@@ -60,5 +60,48 @@ class InterpreterTestCase(unittest.TestCase):
         parse(shell % 'x = "abc"; x.replace("b", "");').execute(context)
         self.assertEqual(context.root_frame.values["x"], "ac")
 
+    def test_evaluate_numbers(self):
+        context = Context()
+        parse(shell % 'x = 1000; x = x * -0.1;').execute(context)
+        self.assertEqual(context.root_frame.values["x"], -100.0)
+
+    def test_comments(self):
+        context = Context()
+        parse(shell % 'x = 1000; x = x * -0.1; x = x + 10; x = x - 1;').execute(context)
+        self.assertEqual(context.root_frame.values["x"], -91.0)
+
+        context = Context()
+        parse(shell % 'x = 1000; x = x * -0.1; /* x = x + 10; */ x = x - 1;').execute(context)
+        self.assertEqual(context.root_frame.values["x"], -101.0)
+
+        context = Context()
+        parse(shell % 'x = 1000; x = x * -0.1; # x = x + 10; \n x = x - 1;').execute(context)
+        self.assertEqual(context.root_frame.values["x"], -101.0)
+
+        context = Context()
+        parse(shell % 'x = 1000; x = x * -0.1; // x = x + 10; \n x = x - 1;').execute(context)
+        self.assertEqual(context.root_frame.values["x"], -101.0)
+
+    def test_if_then_else(self):
+        context = Context()
+        parse(shell % 'x = 1000; if (x < 10) { x = x * 100; } else { x = x / 100; }').execute(context)
+        self.assertEqual(context.root_frame.values["x"], 10.0)
+
+    def test_error_location(self):
+        with self.assertRaises(Exception) as pe:
+            parse(shell % 'for (i=0; i < 1000; i = i +) { a = 100; }')
+        self.assertEqual(pe.exception.loc, 35)
+
+    def test_array_add(self):
+        context = Context()
+        parse(shell % 'x = ["a"] + ["a"];').execute(context)
+        self.assertEqual(context.root_frame.values["x"], ["a", "a"])
+
+    def test_array_reference(self):
+        context = Context()
+        parse(shell % 'x = ["a"] + ["a"]; x = x[1];').execute(context)
+        self.assertEqual(context.root_frame.values["x"], "a")
+
+
 if __name__ == "__main__":
     unittest.main()
